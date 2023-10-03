13059bda7ed632a3b7343cc5b608a3ef7529b50f
a348775 <a348775@uach.mx>
2023-10-03 12:00:46 -0600

    se complementaron los menus de agiles y tradicionales

--- scriptbash.sh
+++ scriptbash.sh
@@ -2,30 +2,70 @@
 
 while true; do
     echo "Seleccione una opción:"
-    echo "-a: Metodologias Agiles"
+    echo "-a: Agile"
     echo "-t: Metodologías tradicionales"
     echo "q: Salir"
     read opcion
 
     case $opcion in
         -a)
-            echo "Guía rápida de Agile, seleccione un tema (1, 2, 3, 4):"
+            echo "Bienvenido a la guía rápida de Agile, seleccione un tema:"
             echo "1. SCRUM"
             echo "2. X.P."
             echo "3. Kanban"
             echo "4. Crystal"
             read tema
 
-
+            case $tema in
+                1)
+                    seccion="SCRUM"
+                    archivo="scrum.inf"
+                    ;;
+                2)
+                    seccion="X.P."
+                    archivo="xp.inf"
+                    ;;
+                3)
+                    seccion="Kanban"
+                    archivo="kanban.inf"
+                    ;;
+                4)
+                    seccion="Crystal"
+                    archivo="crystal.inf"
+                    ;;
+                *)
+                    echo "Opción no válida"
+                    continue
+                    ;;
+            esac
+            ;;
 
         -t)
-            echo "Guía rápida de metodologías tradicionales, seleccione un tema: (1, 2, 3):"
+            echo "Bienvenido a la guía rápida de metodologías tradicionales, seleccione un tema:"
             echo "1. Cascada"
             echo "2. Espiral"
             echo "3. Modelo V"
             read tema
 
-
+            case $tema in
+                1)
+                    seccion="Cascada"
+                    archivo="cascada.inf"
+                    ;;
+                2)
+                    seccion="Espiral"
+                    archivo="espiral.inf"
+                    ;;
+                3)
+                    seccion="Modelo V"
+                    archivo="modelo_v.inf"
+                    ;;
+                *)
+                    echo "Opción no válida"
+                    continue
+                    ;;
+            esac
+            ;;
 
         q)
             exit 0
@@ -36,4 +76,3 @@ while true; do
             continue
             ;;
     esac
-;;
